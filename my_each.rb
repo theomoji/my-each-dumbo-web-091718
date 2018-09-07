4ad2b5245472ec2157e78b329067d10cbf9f0fcb
Shuo Wang <gken90@gmail.com>
2018-09-07 23:22:12 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
 end
+
+
+
+# ```ruby
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#   puts i
+# end
+
+
+# def yielding
+#   puts "the program is executing the code inside the method"
+#   yield
+#   puts "now we are back in the method"
+# end
+#
+# the program is executing the code inside the method
+# the method has yielded to the block!
+# now we are back in the method
