73fc954bee629fb318055dc8f6cb8b3a866943e6
Shuo Wang <gken90@gmail.com>
2018-09-08 01:44:53 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -7,9 +7,3 @@ end
 
 my_each(array) do
   |element| puts element
-
-
-
-
-
-# You'll be building out the content of the #my_each method. This method should accept an argument of an array and use the while loop to iterate over each member of that array, yielding each element contained in the array to a block.
